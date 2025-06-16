4a9e2cec51b5d02091aa2e84f1b005ba313848a9
Simon <simon>
2025-06-17 00:25:58 +0200

    first draft

--- src/routes/writer/+page.svelte
+++ src/routes/writer/+page.svelte
@@ -1,24 +1,113 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import type { Attachment } from 'svelte/attachments';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 
+	import { onMount } from 'svelte';
+	import jspreadsheet from 'jspreadsheet-ce';
 
-    async function parse() {
-    const svgText = await fetch('image.svg').then(i => i.text());
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(svgText, "image/svg+xml");
-    const texts   = Array.from(doc.querySelectorAll("text"));
-    const images  = Array.from(doc.querySelectorAll("image"));
-    console.log(images);
-    console.log(texts.map(t => {
-        return {
-            id: t.id,
-            content: t.textContent
-        }     
-    }
-    ))
-    }
+	interface column {
+		type:
+			| 'text'
+			| 'numeric'
+			| 'hidden'
+			| 'dropdown'
+			| 'autocomplete'
+			| 'checkbox'
+			| 'radio'
+			| 'calendar'
+			| 'image'
+			| 'color'
+			| 'html'
+			| CustomEditor
+			| undefined;
+		title: string;
+		width: number;
+	}
 
-    onMount(() => {
-        parse();
-    })
+	let columns: column[] = $state([]);
+	let data: string[][] = $state([[]]);
+	let spreadsheet = $state(null);
+	let svgTemplate: SVGSVGElement;
+
+	async function parse() {
+		const svgText = await fetch('image.svg').then((i) => i.text());
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(svgText, 'image/svg+xml');
+		svgTemplate = doc.documentElement as SVGSVGElement;
+		const texts = Array.from(doc.querySelectorAll('text'));
+		const images = Array.from(doc.querySelectorAll('image'));
+		const textColumns = texts.map((t) => {
+			return {
+				title: t.id,
+				type: 'text',
+				width: 100
+			};
+		});
+		columns = textColumns;
+		data = [texts.map((t) => t.textContent || '')];
+		initSpreadsheet();
+	}
+
+	function generateSVG(row: string[]): SVGSVGElement | null {
+		if (!svgTemplate) {
+			return null;
+		}
+		const svg = svgTemplate.cloneNode(true) as SVGSVGElement;
+
+		columns.forEach((col, idx) => {
+			const el = svg.getElementById(col.title);
+			if (el && el.tagName === 'text') {
+				el.textContent = row[idx] || '';
+			}
+		});
+
+		return svg;
+	}
+
+	function initSpreadsheet() {
+		const spreadsheet = jspreadsheet(document.getElementById('spreadsheet') as HTMLDivElement, {
+			worksheets: [
+				{
+					data,
+					columns
+				}
+			]
+		});
+	}
+
+	onMount(() => {
+		parse();
+	});
+
+	function attachSVG(svg: SVGSVGElement | null): Attachment {
+		return (element) => {
+			if (svg instanceof Node) {
+				element.appendChild(svg);
+				const { x, y, width, height } = svg.getBBox();
+				svg.setAttribute('viewBox', `${x} ${y} ${width} ${height}`);
+				Object.assign(svg.style, {
+					display: 'block',
+					width: '100%',
+					height: '100%'
+				});
+			}
+			return () => {
+				element.removeChild(svg);
+			};
+		};
+	}
 </script>
+
+{#if data && data.length}
+	<ScrollArea class="w-screen rounded-md border whitespace-nowrap" orientation="horizontal">
+		<div class="flex w-full">
+			{#each data as row (row)}
+				{@const svg = generateSVG(row)}
+				{#if svg}
+					<div class="h-full w-90" {@attach attachSVG(svg)}></div>
+				{/if}
+			{/each}
+		</div>
+	</ScrollArea>
+{/if}
+<div id="spreadsheet"></div>
