b36c0ce12e42393a443af9a98900b51a97fe839c
simon <simonhackler@gmail.com>
2025-12-26 10:26:27 +0100

    fixed async

--- packages/app/src/routes/games/[gameName]/play/+page.svelte
+++ packages/app/src/routes/games/[gameName]/play/+page.svelte
@@ -27,12 +27,12 @@
 	import { HandContainer } from './HandContainer';
 	import { Position } from './frontend-components/position';
 	import { FrontendStack } from './frontend-components/frontend-stack';
-	import { assert } from '$lib/utils/assert';
+	import { assert, requireParam } from '$lib/utils/assert';
+	import type { Attachment } from 'svelte/attachments';
 
 	let canvasContainer: HTMLDivElement;
-	let app: Application;
 
-	const projectName = $derived(page.params.gameName);
+	const projectName = $derived(requireParam('gameName'));
 	const cardName = $derived(page.params.deckName || 'western');
 	const fileSystem = getFileSystemContext();
 
@@ -46,7 +46,7 @@
 	}
 
 	const client = new Client('ws://localhost:2567');
-	let room: Room<BoardGameRoomState>;
+	// let room: Room<BoardGameRoomState>;
 
 	let syncCards: Map<string, BoardGameItem> = new Map();
 	let positions: Map<string, Position> = new Map();
@@ -90,7 +90,9 @@
 
 	let boardContainer: Container;
 	let handContainer: HandContainer;
-	let hybridResults: ParsedSvg[];
+	// let hybridResults: ParsedSvg[];
+
+	// const hybridResults = $derived(await loadAndProcessCards(projectName, cardName, fileSystem));
 
 	let viewport: Viewport;
 
@@ -133,15 +135,8 @@
 		return payload;
 	}
 
-	onMount(async () => {
-		// Disable native context menu on the entire page
-		const handleContextMenu = (e: Event) => {
-			e.preventDefault();
-		};
-		document.addEventListener('contextmenu', handleContextMenu);
-
-		// Listen for card array changes
-		app = new Application();
+	async function initApp() {
+		const app = new Application();
 		await app.init({
 			background: '#eba92e',
 			resizeTo: window,
@@ -149,23 +144,15 @@
 			resolution: window.devicePixelRatio || 1,
 			autoDensity: true
 		});
-
-		const previewer = new PreviewHelper(app);
-		previewer.previewContainer.zIndex = 10000;
-
-        assert(projectName != undefined, 'Project name is undefined');
-		hybridResults = await loadAndProcessCards(projectName, cardName, fileSystem);
-
 		initDevtools({ app });
 		window.__PIXI_DEVTOOLS__ = {
 			app
 		};
 		console.log('App renderer size:', app.renderer.width, app.renderer.height);
 		viewport = createViewport(app);
+		// passing app here feels wrong. It is needed to render textures. Ideally classes in here shouldn't have to know about app
+		const previewer = new PreviewHelper(app);
 
-		canvasContainer.appendChild(app.canvas);
-
-		// allow the stage itself to catch pointer events anywhere
 		app.stage.eventMode = 'static';
 		app.stage.hitArea = app.screen;
 
@@ -242,7 +229,6 @@
 					});
 				}
 			} else if (drag.dragType == 'handToBoard') {
-				// Handle playing card from hand to board
 				const containers = selectionManager.values();
 				for (const c of containers) {
 					if (handContainer.hasItem(c)) {
@@ -390,29 +376,13 @@
 				drag = null;
 			}
 		});
-		window.addEventListener('keydown', (e) => {
-			if (e.key === 'Escape') {
-				closeContextMenu();
-			} else if (e.code === 'KeyF') {
-				selectionManager.forEach((item) => handleFlipCard(item));
-			} else if (e.code === 'KeyD') {
-				selectionManager.forEach((item) => handleDrawCard(item));
-			} else if (e.code == 'AltLeft') {
-				if (hoverItem) {
-					previewer.showPreview(hoverItem);
-					pressedKeys.add('AltLeft');
-				}
-			}
-		});
+		return app;
+	}
 
-		window.addEventListener('keyup', (e) => {
-			if (e.code == 'AltLeft') {
-				pressedKeys.delete('AltLeft');
-				previewer.hidePreview();
-			}
-		});
+    async function createRoom(_app: Application) {
+	    const hybridResults = await loadAndProcessCards(projectName, cardName, fileSystem)
+		const room = await createOrJoinRoom(client, 'my_room');
 
-		room = await createOrJoinRoom(client, 'my_room');
 		room.send('cmd', {
 			commandType: 'init',
 			payload: parsePayload(hybridResults)
@@ -422,11 +392,312 @@
 		s(room.state).components.onAdd((component, _index) => {
 			initComponent(hybridResults, component, room.state, s);
 		});
+        return room;
+    }
 
-		return () => {
-			app?.destroy(true, { children: true, texture: true });
+	const app = $state(await initApp());
+    const room = $derived(await createRoom(app));
+
+	onMount(async () => {
+		// Disable native context menu on the entire page
+		const handleContextMenu = (e: Event) => {
+			e.preventDefault();
 		};
+		document.addEventListener('contextmenu', handleContextMenu);
+
+		// app = new Application();
+		// await app.init({
+		// 	background: '#eba92e',
+		// 	resizeTo: window,
+		// 	antialias: true,
+		// 	resolution: window.devicePixelRatio || 1,
+		// 	autoDensity: true
+		// });
+		//
+		// const previewer = new PreviewHelper(app);
+		// previewer.previewContainer.zIndex = 10000;
+		//
+		// initDevtools({ app });
+		// window.__PIXI_DEVTOOLS__ = {
+		// 	app
+		// };
+		// console.log('App renderer size:', app.renderer.width, app.renderer.height);
+		// viewport = createViewport(app);
+		//
+		//
+		// app.stage.eventMode = 'static';
+		// app.stage.hitArea = app.screen;
+		//
+		// boardContainer = new Container();
+		// viewport.addChild(boardContainer);
+		//
+		// const screenContainer = new Container({
+		// 	layout: {
+		// 		width: app.screen.width,
+		// 		height: app.screen.height,
+		// 		flexDirection: 'column',
+		// 		justifyContent: 'flex-end',
+		// 		alignItems: 'center'
+		// 	}
+		// });
+		//
+		// app.stage.addChild(screenContainer);
+		//
+		// handContainer = new HandContainer();
+		// screenContainer.addChild(handContainer.container);
+		//
+		// const width = 200;
+		// const height = 200;
+		//
+		// const marquee = new MarqueeSelection({
+		// 	dash: 4,
+		// 	dashSpace: 4,
+		// 	thickness: 1,
+		// 	speed: 0.2,
+		// 	width,
+		// 	height,
+		// 	color: 'black',
+		// 	x: ((app.screen.width - width) / 2) | 0,
+		// 	y: ((app.screen.height - height) / 2) | 0
+		// });
+		// marquee.visible = false;
+		//
+		// function endDrag(e: FederatedPointerEvent) {
+		// 	if (!drag) return;
+		//
+		// 	if (e.button === 2) {
+		// 		drag = null;
+		// 		return;
+		// 	}
+		//
+		// 	if (drag.dragType == 'marquee') {
+		// 		marquee.visible = false;
+		// 		const global = app.stage.toGlobal(marquee.position);
+		// 		const selectionRect = new Rectangle(
+		// 			Math.min(global.x, e.globalX),
+		// 			Math.min(global.y, e.globalY),
+		// 			marquee.width,
+		// 			marquee.height
+		// 		);
+		// 		for (const item of syncCards.values()) {
+		// 			const itemBounds = item.getBounds();
+		// 			if (selectionRect.intersects(new Rectangle().copyFromBounds(itemBounds))) {
+		// 				selectionManager.select(item);
+		// 			}
+		// 		}
+		// 	} else if (drag.dragType == 'selection') {
+		// 		const containers = selectionManager.values();
+		// 		for (const c of containers) {
+		// 			const cardId = c.id;
+		// 			c.cursor = 'pointer';
+		//
+		// 			room.send('cmd', {
+		// 				commandType: 'moveend',
+		// 				payload: {
+		// 					cardId: cardId,
+		// 					x: c.x,
+		// 					y: c.y
+		// 				}
+		// 			});
+		// 		}
+		// 	} else if (drag.dragType == 'handToBoard') {
+		// 		const containers = selectionManager.values();
+		// 		for (const c of containers) {
+		// 			if (handContainer.hasItem(c)) {
+		// 				c.x = 0;
+		// 				c.y = 0;
+		// 			} else {
+		// 				handlePlayCard(c);
+		// 			}
+		// 			c.cursor = 'pointer';
+		// 		}
+		// 	}
+		// 	drag = null;
+		// }
+		//
+		// app.stage.addChild(marquee);
+		//
+		// app.stage.on('pointermove', (e) => {
+		// 	previewer.updatePointer(e.globalX, e.globalY);
+		// 	const topItem = findTopLevelItem(e.target);
+		// 	if (topItem) {
+		// 		hoverItem = topItem;
+		// 		if (pressedKeys.has('AltLeft')) {
+		// 			previewer.showPreview(hoverItem);
+		// 		}
+		// 	} else {
+		// 		previewer.hidePreview();
+		// 	}
+		// 	if (!drag) return;
+		//
+		// 	if (drag.dragType == 'marquee') {
+		// 		const width = e.global.x - drag.startGlobalX + e.globalX - marquee.x;
+		// 		const height = e.global.y - drag.startGlobalY + e.globalY - marquee.y;
+		// 		marquee.resize(width, height);
+		// 	} else if (drag.dragType == 'selection') {
+		// 		const worldPos = viewport.toWorld(e.global);
+		// 		const startWorldPos = viewport.toWorld(drag.startGlobalX, drag.startGlobalY);
+		// 		const delta = worldPos.subtract(startWorldPos);
+		// 		for (const boardItem of selectionManager.values()) {
+		// 			const newPos = boardItem.position.add(delta);
+		// 			boardItem.position = newPos;
+		// 			room.send('cmd', {
+		// 				commandType: 'move',
+		// 				payload: {
+		// 					componentId: boardItem.id,
+		// 					x: boardItem.x,
+		// 					y: boardItem.y
+		// 				}
+		// 			});
+		// 		}
+		// 	} else if (drag.dragType == 'handToBoard') {
+		// 		for (const boardItem of selectionManager.values()) {
+		// 			if (handContainer.hasItem(boardItem)) {
+		// 				const deltaX = e.globalX - drag.startGlobalX;
+		// 				const deltaY = e.globalY - drag.startGlobalY;
+		// 				const wrapper = boardItem.parent;
+		// 				if (wrapper) {
+		// 					const wrapperGlobal = wrapper.toGlobal(boardItem.position);
+		// 					const newGlobalX = wrapperGlobal.x + deltaX;
+		// 					const newGlobalY = wrapperGlobal.y + deltaY;
+		//
+		// 					const newLocal = wrapper.toLocal({ x: newGlobalX, y: newGlobalY });
+		// 					boardItem.position.set(newLocal.x, newLocal.y);
+		//
+		// 					const wrapperBounds = wrapper.getBounds();
+		// 					const handBounds = handContainer.container.getBounds();
+		// 					const cardHeight = wrapperBounds.height;
+		//
+		// 					const cardCenterY = wrapperBounds.y + cardHeight / 2;
+		// 					const handTop = handBounds.maxY - cardHeight;
+		//
+		// 					if (cardCenterY < handTop) {
+		// 						handContainer.removeItem(boardItem);
+		// 						boardItem.scale.set(0.5);
+		// 						boardItem.rotation = 0;
+		// 						boardItem.pivot.set(0, 0);
+		// 						boardItem.alpha = 1.0;
+		// 						const worldPos = viewport.toWorld(e.global);
+		//
+		// 						drag.startGlobalX = e.globalX;
+		// 						drag.startGlobalY = e.globalY;
+		//
+		// 						const offset = e.global.subtract(new Point(wrapperBounds.minX, wrapperBounds.minY));
+		//
+		// 						boardItem.position = worldPos
+		// 							.subtract(offset)
+		// 							.subtract(new Point(wrapperBounds.width / 2, wrapperBounds.height / 2));
+		//
+		// 						boardContainer.addChild(boardItem);
+		// 					}
+		// 				}
+		// 			} else {
+		// 				const worldPos = viewport.toWorld(e.global);
+		// 				const startWorldPos = viewport.toWorld(drag.startGlobalX, drag.startGlobalY);
+		// 				const delta = worldPos.subtract(startWorldPos);
+		// 				const newPos = boardItem.position.add(delta);
+		// 				boardItem.position = newPos;
+		// 			}
+		// 		}
+		// 	}
+		// 	drag.startGlobalX = e.globalX;
+		// 	drag.startGlobalY = e.globalY;
+		// });
+		//
+		// app.stage.on('pointerup', endDrag);
+		// app.stage.on('pointerupoutside', endDrag);
+		// app.stage.on('pointerdown', (e) => {
+		// 	const boardItem = findTopLevelItem(e.target);
+		// 	if (boardItem) {
+		// 		if (e.ctrlKey) {
+		// 			if (selectionManager.has(boardItem) && e.button === 1) {
+		// 				selectionManager.deselect(boardItem);
+		// 			} else {
+		// 				selectionManager.select(boardItem);
+		// 			}
+		// 		} else {
+		// 			selectionManager.selectOnly(boardItem);
+		// 		}
+		// 	}
+		// 	if (!boardItem) {
+		// 		selectionManager.clear();
+		// 	}
+		//
+		// 	// Determine drag type based on whether the item is in hand
+		// 	let dragType: 'marquee' | 'selection' | 'handToBoard' = 'selection';
+		// 	if (boardItem && handContainer.hasItem(boardItem)) {
+		// 		dragType = 'handToBoard';
+		// 		// Don't immediately move card from hand - let it stay until more than half is dragged out
+		// 	}
+		//
+		// 	drag = {
+		// 		startGlobalX: e.globalX,
+		// 		startGlobalY: e.globalY,
+		// 		clickThreshold: 10,
+		// 		dragType: dragType
+		// 	};
+		// 	if (e.button === 2 && boardItem) {
+		// 		handleRightClick(e);
+		// 	}
+		// 	if (e.shiftKey) {
+		// 		drag.dragType = 'marquee';
+		// 		marquee.position.copyFrom(e.global);
+		// 		marquee.visible = true;
+		// 		marquee.resize(2, 2);
+		// 	} else if (!boardItem || (!selectionManager.has(boardItem) && dragType !== 'handToBoard')) {
+		// 		drag = null;
+		// 	}
+		// });
+		//
+		// window.addEventListener('keydown', (e) => {
+		// 	if (e.key === 'Escape') {
+		// 		closeContextMenu();
+		// 	} else if (e.code === 'KeyF') {
+		// 		selectionManager.forEach((item) => handleFlipCard(item));
+		// 	} else if (e.code === 'KeyD') {
+		// 		selectionManager.forEach((item) => handleDrawCard(item));
+		// 	} else if (e.code == 'AltLeft') {
+		// 		if (hoverItem) {
+		// 			previewer.showPreview(hoverItem);
+		// 			pressedKeys.add('AltLeft');
+		// 		}
+		// 	}
+		// });
+		//
+		// window.addEventListener('keyup', (e) => {
+		// 	if (e.code == 'AltLeft') {
+		// 		pressedKeys.delete('AltLeft');
+		// 		previewer.hidePreview();
+		// 	}
+		// });
+		//
+		// canvasContainer.appendChild(app.canvas);
+
+		// room = await createOrJoinRoom(client, 'my_room');
+		// room.send('cmd', {
+		// 	commandType: 'init',
+		// 	payload: parsePayload(hybridResults)
+		// });
+		// let s = getStateCallbacks(room);
+		//
+		// s(room.state).components.onAdd((component, _index) => {
+		// 	initComponent(hybridResults, component, room.state, s);
+		// });
+		//
+		// return () => {
+		// 	app?.destroy(true, { children: true, texture: true });
+		// };
 	});
+
+	function attachApp(app: Application): Attachment {
+		return (element) => {
+			element.appendChild(app.view);
+			return () => {
+			    app.destroy(true, { children: true, texture: true });
+				element.removeChild(app.view);
+			};
+		};
+	}
 
 	async function initComponent(
 		hybridResults: ParsedSvg[],
@@ -513,7 +784,7 @@
 </script>
 
 <div class="absolute inset-0">
-	<div bind:this={canvasContainer} class="full relative w-full" style="pointer-events: auto;"></div>
+	<div bind:this={canvasContainer} class="full relative w-full" style="pointer-events: auto;" {@attach attachApp(app)}></div>
 
 	<ContextMenu.Root bind:open={showContextMenu}>
 		<ContextMenu.Trigger
