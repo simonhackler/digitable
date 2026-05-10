af5488bdfbe5e5a0786f3a5c06174d0e735f738f
Simon Hackler <simonhackler@gmail.com>
2026-05-10 15:31:22 +0200

    home page

---
+++ packages/db/src/schema/app-users.ts
@@ -0,0 +1,8 @@
+import { text, integer, pgTable } from "drizzle-orm/pg-core";
+import { user } from "./auth";
+
+export const appUser = pgTable('app_user', {
+    id: integer('id').primaryKey(),
+    authId: text('auth_id').notNull().references(() => user.id, {onDelete: 'cascade'})
+    checkedTermsAndConditionsAndPrivacyPolicy:
+})
